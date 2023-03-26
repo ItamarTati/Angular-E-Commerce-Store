39bfa9227219783e68dd3a6a241c76e29ecfa142
Itamat Tati <itamartati1994@gmail.com>
2023-03-26 03:02:09 +0100

    add test env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  stripeKey: process.env["STRIPE_KEY"],
+  stripeKey: 'pk_test_51MpIgOD0lmFpNKfWPGz1H7Ah7PfiS737Luh8PYVrrZoMMstxeq48mWdES47YSC3UHMvMWgjhTYj7ndET1lJ9OECc00h7dZywSB',
   auth: {
-    clientId: process.env["CLIENT_ID"],
-    domain: process.env["DOMAIN"]
+    clientId: 'I9UunIpmYZz52QRFUsZW9dBrRSSI7gZl',
+    domain: 'dev-z1snagqpobiokflv.uk.auth0.com'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  stripeKey: process.env["STRIPE_KEY"],
+  production: false,
+  stripeKey: 'pk_test_51MpIgOD0lmFpNKfWPGz1H7Ah7PfiS737Luh8PYVrrZoMMstxeq48mWdES47YSC3UHMvMWgjhTYj7ndET1lJ9OECc00h7dZywSB',
   auth: {
-    clientId: process.env["CLIENT_ID"],
-    domain: process.env["DOMAIN"]
+    clientId: 'I9UunIpmYZz52QRFUsZW9dBrRSSI7gZl',
+    domain: 'dev-z1snagqpobiokflv.uk.auth0.com'
   }
 };
 /*
